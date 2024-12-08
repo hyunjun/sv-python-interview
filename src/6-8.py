4b2bf01d9be1f32db534b050e056f46216b7264e
Do-yoon <1.kim.doyoon@gmail.com>
2024-12-08 13:38:10 +0900

    코드 6-8: 인덴트 수정, 라이브러리 추가, 변수 수정

--- src/6-8.py
+++ src/6-8.py
@@ -1,14 +1,16 @@
+from collections import defaultdict
+from typing import List
+
+
 class Solution:
-
-
-def subarraySum(self, nums: List[int], K: int) -> int:
-    table = defaultdict(int)
-    res, presume = 0, 0
-    table[0] = 1
-    for i, num in enumerate(nums):
-        presum += num
-        if presum-K in table:  # 현재 추정-K가 딕셔너리에 있는 경우
-            res += table[presum-K]
-            # 현재까지의 합산 수를 업데이트한다.
-            table[presum] += 1
-    return res
+    def subarraySum(self, nums: List[int], K: int) -> int:
+        table = defaultdict(int)
+        res, presum = 0, 0
+        table[0] = 1
+        for i, num in enumerate(nums):
+            presum += num
+            if presum-K in table:  # 현재 추정-K가 딕셔너리에 있는 경우
+                res += table[presum-K]
+                # 현재까지의 합산 수를 업데이트한다.
+                table[presum] += 1
+        return res
