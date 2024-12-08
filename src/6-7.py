ede0f61f382c4ab12d70412b1270e8813f38be3a
Do-yoon <1.kim.doyoon@gmail.com>
2024-12-08 13:18:20 +0900

    코드 6-7: 출력 텍스트 수정

--- src/6-7.py
+++ src/6-7.py
@@ -1,7 +1,7 @@
 # 딕셔너리 만들기
-Dict = {1: "Geeks", "name": "For", 3: "Geeks"}
+Dict = {1: 'Geeks', "name": 'For', 3: 'Geeks'}
 
 # pop() 함수를 사용하여 키 값을 삭제한다.
 pop_ele = Dict.pop(1)
-print("\nDictionary after deletion: " + str(Dict))
-print("Value associated to popped key is: " + str(pop_ele))
+print('Dictionary after deletion: ' + str(Dict))
+print('Value associated to popped key is: ' + str(pop_ele)) 
