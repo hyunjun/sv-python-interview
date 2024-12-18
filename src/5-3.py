78b5d963c4f9e9de61646e52489fdd4d9f30121d
Do-yoon <1.kim.doyoon@gmail.com>
2024-12-18 18:21:41 +0900

    주석 수정

--- src/5-3.py
+++ src/5-3.py
@@ -7,5 +7,5 @@ customers.put((3, "Charles"))
 customers.put((1, "Riya"))
 customers.put((4, "Stacy"))
 while customers:
-    print(customers.get())
     # Riya, Harry, Charles, Stacy 순으로 이름을 출력한다.
+    print(customers.get())
