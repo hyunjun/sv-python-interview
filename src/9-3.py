820c14eea630d2886b906eff75d178fc863916cd
Do-yoon <1.kim.doyoon@gmail.com>
2024-12-19 16:39:09 +0900

    개행 수정

--- src/9-3.py
+++ src/9-3.py
@@ -2,7 +2,6 @@ class Solution:
     def inorderTraversal(self, root):
         if not root:
             return []
-        return
-        self.inorderTraversal(root.left)
-        + [root.val]
-        + self.inorderTraversal(root.right)
+        return (self.inorderTraversal(root.left)
+                + [root.val]
+                + self.inorderTraversal(root.right))
