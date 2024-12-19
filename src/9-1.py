a29783fc596619857aec4e37cc4aefb9bb207229
Do-yoon <1.kim.doyoon@gmail.com>
2024-12-19 16:23:00 +0900

    인덴트, 주석 수정

--- src/9-1.py
+++ src/9-1.py
@@ -1,5 +1,6 @@
 class Solution:
-    def preorderTraversal(self, root):  # 전위순회
+    def preorderTraversal(self, root):  # 전위 순회
         if not root:
-        return []
-    return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+            return []
+        return [root.val] + self.preorderTraversal(root.left) \
+    + self.preorderTraversal(root.right)
