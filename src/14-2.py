23c0bbde4396b634e384e9240252e291952fd466
Do-yoon <1.kim.doyoon@gmail.com>
2024-12-25 20:36:25 +0900

    타입 힌트 수정

--- src/14-2.py
+++ src/14-2.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit(self, prices: list[int]) -> int:
         min_price, profit = inf, 0
         for price in prices:
             min_price = min(min_price, price)
