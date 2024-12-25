256336f07308dda30f7bf47f42db76ed91581f81
Do-yoon <1.kim.doyoon@gmail.com>
2024-12-26 00:49:32 +0900

    타입 힌트 수정

--- src/19-5.py
+++ src/19-5.py
@@ -1,4 +1,4 @@
-def CountGroups(self, circles: List[Circle]) -> int:
+def CountGroups(self, circles: list[Circle]) -> int:
     """원 목록이 주어지면 원 그룹의 수를 반환한다."""
     total_groups = 0
     visited = set()
