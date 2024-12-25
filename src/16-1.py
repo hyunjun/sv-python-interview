d37925f45bffb879f07ec92f4edc6baf4c8f6527
Do-yoon <1.kim.doyoon@gmail.com>
2024-12-25 21:54:29 +0900

    타입 힌트 수정, 주석 수정

--- src/16-1.py
+++ src/16-1.py
@@ -26,7 +26,7 @@ class Solution(object):
     def emptySlots(self, board, rows, cols):
         empty = []
 
-        # 리스트에 공간의 위치좌표를 추가한다.
+        # 리스트에 공간의 위치 좌표를 추가한다.
         for i in range(rows):
             for j in range(cols):
                 if board[i][j] == ".":
@@ -39,24 +39,24 @@ class Solution(object):
         if start >= N:
             return True
 
-        # 현재 공간 위치의 좌표를 가져온다.
+        # 현재 공간의 위치 좌표를 가져온다.
         x = empty[start][0]
         y = empty[start][1]
 
         # 9개 값을 통해 반복
         for k in range(1, 10):
-            # 현재 값이 요구 사항을 충족하는지 확인
+            # 현잿값이 요구 사항을 충족하는지 확인
             if self.isValid(board, x, y, rows, cols, str(k)):
                 board[x][y] = str(k)  # 해당 스도쿠 위치에 값 할당
                 if self.DFS(board, empty, start + 1, N, rows, cols):
                     return True
-        # 역 추적
+        # 역추적
         board[x][y] = "."
         return False
 
     def solveSudoku(self, board):
         """
-        :type board : List[List[str]]
+        :type board : list[list[str]]
         :rtype: None 아무것도 반환하지 않고 대신 보드를 제자리에서 수정한다.
         """
 
