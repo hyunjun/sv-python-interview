28681b4fc161e2900a61b38fb3f8c38c2191f546
hyunjun.chung <hyunjun.chung@gmail.com>
2025-03-14 12:50:53 +0900

    Add equal sign (previous version doesn't work properly)

--- src/12-3.py
+++ src/12-3.py
@@ -12,11 +12,11 @@ class ConferenceBooking:
         if event.start > event.end:
             return False
         low, high = 0, len(self.schedules) - 1
-        while low < high:
+        while low <= high:
             mid = (low + high) // 2
-            if event.start > self.schedules[mid].end:
+            if event.start >= self.schedules[mid].end:
                 low = mid + 1
-            elif event.end < self.schedules[mid].start:
+            elif event.end <= self.schedules[mid].start:
                 low = mid - 1
             else:
                 return False
@@ -31,7 +31,6 @@ if __name__ == "__main__":
     assert res == True
     event2 = Event(20, 30)
     res = solution.booking(event2)
-
     assert res == True
     event3 = Event(10, 15)
     res = solution.booking(event3)
