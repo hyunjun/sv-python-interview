954373043279f971603616ad7499ffe9a309e09e
hyunjun.chung <hyunjun.chung@gmail.com>
2025-03-14 12:50:06 +0900

    Use empty() to prevent infite loop

--- src/5-3.py
+++ src/5-3.py
@@ -6,6 +6,6 @@ customers.put((2, "Harry"))
 customers.put((3, "Charles"))
 customers.put((1, "Riya"))
 customers.put((4, "Stacy"))
-while customers:
+while not customers.empty():
     # Riya, Harry, Charles, Stacy 순으로 이름을 출력한다.
     print(customers.get())
